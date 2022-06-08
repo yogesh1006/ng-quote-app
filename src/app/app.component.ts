5432f67ff94aea55efa9b02f5356683dac80d411
yogesh1006 <yogeshkasliwal707@gmail.com>
2022-06-09 00:52:14 +0530

    lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,11 @@ import { User } from 'src/models/user';
 
 export class AppComponent {
   title:string = 'header';
+  show = true;
   users: Array<User> =[
     {name:"yogesh",age:25, status:"single",salary:2500,img:"assets/u1.jpg"},
-    {name:"ramesh",age:45, status:"married",salary:3500,img:"assets/u2.jpg"},
-    {name:"suresh",age:35, status:"commited",salary:5000,img:"assets/u3.png"}
+    // {name:"ramesh",age:45, status:"married",salary:3500,img:"assets/u2.jpg"},
+    // {name:"suresh",age:35, status:"commited",salary:5000,img:"assets/u3.png"}
   ]
 
   receivedData(e:any){
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,21 +1,53 @@
-import { Component, Input , Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit, OnChanges, OnDestroy {
 
-  @Input() name!:string;
-  @Input() age!:number;
-  @Input() status!:string;
-  @Input() salary!:number;
-  @Input() img!:string;
+  @Input() name!: string;
+  @Input() age!: number;
+  @Input() status!: string;
+  @Input() salary!: number;
+  @Input() img!: string;
+  @Input() title!: string;
 
-  @Output() myevent= new EventEmitter<string>();
+  @Output() myevent = new EventEmitter<string>();
 
-  passData(){
+  passData() {
     this.myevent.emit("Learning Angular")
+  }
+
+  listnerRef = setInterval(() => { }, 1000)
+
+  // 1 constructor call hota hai jab class kaa instance create hota hai
+  constructor() {
+    console.log('constructor', this.name);
+    // properties initialized kr skte hai
+  }
+
+  // 2 this method has an access to all properties we defined 
+  // whenever any property will change this method will get called
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngonChnages', changes);
+  }
+
+  // 3 ye call hota hai jab component pura ready ho jata hai tb
+  ngOnInit() {
+    console.log('ngoninit', this.name);
+    this.listnerRef = setInterval(() => {
+      console.log('timer running....');
+    }, 2000)
+    // properties
+    // event listener register
+    // initial data fetch - just like componentDidMount
+  }
+ 
+  //use when we want destroy component from dom
+  ngOnDestroy() {
+    console.log('component destroyed');
+      clearInterval(this.listnerRef)
   }
 }
